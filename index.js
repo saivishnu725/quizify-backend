30f334eeac425dc67b36762f629ede6e6bed94ca
saivishnu725 <saivishnu725@gmail.com>
2024-06-17 13:55:21 +0530

    shit ton of cors things to try anything

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.options('*', cors());
+
 app.use(express.json());
 
 const frontend_url = process.env.FRONTEND || "http://localhost:4000";
